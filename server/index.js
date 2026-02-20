d6f9785f4a282ef625a5fbe285f5091a17b4671a
Kancheti Devieswar <kanchetidevieswar@Kanchetis-MacBook-Pro.local>
2026-02-20 12:55:02 +0530

    Moved admin to server

--- server/index.js
+++ server/index.js
@@ -6,29 +6,23 @@ const path = require("path");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
-mongoose
-    .connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 
-// Routes
 const orderRoutes = require("./routes/orderroute");
 app.use("/api/orders", orderRoutes);
 
-// Serve frontend (important for Render)
-const clientPath = path.join(__dirname, "../client");
-
-app.use(express.static(clientPath));
-
 app.get("/", (req, res) => {
-    res.sendFile(path.join(clientPath, "index.html"));
+    res.json({ message: "Delight Dhaba API Running 🚀" });
 });
 
-// Port
+app.get("/admin", (req, res) => {
+    res.sendFile(path.join(__dirname, "admin.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
