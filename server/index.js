415e28cad1608c021e825307afeee379eca7b11c
Kancheti Devieswar <kanchetidevieswar@Kanchetis-MacBook-Pro.local>
2026-02-20 15:19:03 +0530

    Fixed corrupted index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -8,8 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 🔥 ADD THIS LINE
-app.use(express.static(__dirname));
+// 🔥 Serve static files from server folder
+app.use(express.static(path.join(__dirname)));
 
 app.use("/api/orders", require("./routes/orderroute"));
 
@@ -19,7 +20,10 @@ app.get("/", (req, res) => {
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
+    .catch(err => console.log("Mongo error:", err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+
+app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+});
