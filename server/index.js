18cf9fe7c42b10ac14a919bda3d20bd24ba23896
Kancheti Devieswar <kanchetidevieswar@Kanchetis-MacBook-Pro.local>
2026-02-20 15:13:41 +0530

    Serve static admin file

--- server/index.js
+++ server/index.js
@@ -5,26 +5,21 @@ require("dotenv").config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// ROUTES
+// 🔥 ADD THIS LINE
+app.use(express.static(__dirname));
+
 app.use("/api/orders", require("./routes/orderroute"));
 
-// Test route
 app.get("/", (req, res) => {
     res.send("Delight Dhaba Backend Running");
 });
 
-// Mongo connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log("Mongo Error:", err));
+    .catch(err => console.log(err));
 
-// IMPORTANT: Use Render port
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log("Server running on port", PORT));
