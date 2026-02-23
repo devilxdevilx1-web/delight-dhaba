617ac25c951a0967afcdf63865a96d7d23d0b46b
Kancheti Devieswar <kanchetidevieswar@Kanchetis-MacBook-Pro.local>
2026-02-23 12:17:29 +0530

    Fixed models path

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
 
@@ -9,8 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/* Serve Static Files */
-app.use(express.static(__dirname));
+/* Serve frontend from server folder */
+app.use(express.static(path.join(__dirname)));
 
 /* Routes */
 app.use("/api/auth", require("./routes/authroute"));
@@ -19,7 +20,7 @@ app.use("/api/admin", require("./routes/adminroute"));
 
 /* Root route */
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/app.html");
+    res.sendFile(path.join(__dirname, "app.html"));
 });
 
 /* MongoDB */
@@ -28,5 +29,5 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.log(err));
 
 /* Server */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log("Server running on port", PORT));
