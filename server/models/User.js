035731e9fbf50365234a99513644f60786e738a6
Kancheti Devieswar <kanchetidevieswar@Kanchetis-MacBook-Pro.local>
2026-02-23 09:23:44 +0530

    Full login signup system

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: {
-        type: String,
-        unique: true,
-        sparse: true
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+
+        email: {
+            type: String,
+            unique: true,
+            sparse: true
+        },
+
+        phone: {
+            type: String,
+            unique: true,
+            sparse: true
+        },
+
+        password: { type: String, required: true }
     },
-    phone: {
-        type: String,
-        unique: true,
-        sparse: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-}, { timestamps: true });
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
